5f3fe88c94bc0bafa08929c6174d8020525c3c0d
Austin Jones <ajones53.aj@gmail.com>
2026-07-15 08:32:29 -0400

    turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.TURSO_DB_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
     schema: './src/lib/server/db/schema.ts',
-    dialect: 'sqlite',
-    dbCredentials: { url: process.env.DATABASE_URL },
+    out: './migrations',
+    dialect: 'turso',
+    dbCredentials: {
+        url: process.env.TURSO_DB_URL,
+        authToken: process.env.TURSO_DB_AUTH_TOKEN
+    },
     verbose: true,
     strict: true
 });
