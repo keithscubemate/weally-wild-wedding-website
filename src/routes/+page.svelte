92d6ed5160b342bea0a542003c50652fa60470ab
Austin Jones <ajones53.aj@gmail.com>
2026-07-15 23:27:59 -0400

    style home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
+    import stare from '$lib/assets/cute_stare.jpg';
     let { data } = $props();
 </script>
 
-<h1>Austin & Mariah</h1>
+<div class="hero">
+    <img src={stare} alt="The groom and bride sharing a loving look." />
+    <h1>Austin & Mariah</h1>
+</div>
 
 <div class="info-card">
     <div><b>When:</b> {data.date}</div>
@@ -22,6 +26,35 @@
 <h3>Sunday</h3>
 
 <style>
+    .hero {
+        position: relative;
+        aspect-ratio: 4/3;
+        display: flex;
+        align-items: flex-end;
+        justify-content: left;
+
+        & img {
+            border-radius: 10px;
+            position: absolute;
+            inset: 0;
+            object-position: center 30%;
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+            z-index: 0;
+            box-shadow: 1px 2px 2px var(--border-over-shadow);
+        }
+
+        & h1 {
+            border-radius: 10px;
+            position: relative;
+            margin: 10px;
+            padding: 0.5rem 1.5rem;
+            background: color-mix(in srgb, var(--bg-color) 70%, transparent);
+            font-size: clamp(1.5rem, 5vw, 3rem);
+            z-index: 1;
+        }
+    }
     .info-card div {
         margin: 5px;
     }
